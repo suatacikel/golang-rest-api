2b2e3d8bb491d983b0d5fc430ae3f2f7a45c366f
Suat AÇIKEL <suatacikel@gmail.com>
2021-10-19 10:28:43 +0300

    first api with mux

--- server.go
+++ server.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	const port string = ":8000"
+	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(resp, "Up and running...")
+	})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", addPost).Methods("POST")
+	log.Println("Server listening on port", port)
+	log.Fatalln(http.ListenAndServe(port, router))
+}
